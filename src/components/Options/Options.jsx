425230cabda15e832aae7dccc888c9b4a9bc78e6
Mukuta0Pusmenski <167695339+Mukuta0Pusmenski@users.noreply.github.com>
2025-01-19 17:20:43 +0200

    save-3

--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -2,16 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Options.module.css';
 
-const Options = ({ onLeaveFeedback }) => (
+const Options = ({ onLeaveFeedback, onResetFeedback, totalFeedback }) => (
   <div className="Options">
     <button onClick={() => onLeaveFeedback('good')}>Good</button>
     <button onClick={() => onLeaveFeedback('neutral')}>Neutral</button>
     <button onClick={() => onLeaveFeedback('bad')}>Bad</button>
+    {totalFeedback > 0 && (
+      <button onClick={onResetFeedback}>Reset</button>
+    )}
   </div>
 );
 
 Options.propTypes = {
   onLeaveFeedback: PropTypes.func.isRequired,
+  onResetFeedback: PropTypes.func.isRequired,
+  totalFeedback: PropTypes.number.isRequired,
 };
 
 export default Options;
--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Feedback from '../Feedback/Feedback';
 import Options from '../Options/Options';
+import Notification from '../Notification/Notification';
 import './App.module.css';
 
 const App = () => {
@@ -17,12 +18,26 @@ const App = () => {
     }));
   };
 
+  const resetFeedback = () => {
+    setFeedback({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+  };
+
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+
   return (
     <div className="App">
       <h1>Sip Happens Café</h1>
       <p>Please leave your feedback about our service by selecting one of the options below.</p>
-      <Options onLeaveFeedback={handleFeedback} />
-      <Feedback feedback={feedback} />
+      <Options onLeaveFeedback={handleFeedback} onResetFeedback={resetFeedback} totalFeedback={totalFeedback} />
+      {totalFeedback > 0 ? (
+        <Feedback feedback={feedback} />
+      ) : (
+        <Notification message="No feedback given" />
+      )}
     </div>
   );
 };
